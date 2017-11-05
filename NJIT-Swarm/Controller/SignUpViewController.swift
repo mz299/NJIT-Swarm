c705c6a15e3a58ea1bc5444e349dc0e904d19bf6
Palash Bairagi <Palashbairagi11122@gmail.com>
2017-11-05 17:53:54 -0500

    Validation in Sign Up

--- NJIT-Swarm/Controller/SignUpViewController.swift
+++ NJIT-Swarm/Controller/SignUpViewController.swift
@@ -9,29 +9,35 @@
 import UIKit
 
 class SignUpViewController: UIViewController {
-
+    
     let SIGNUP_TO_HOME_SEGUE = "signupToHome"
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        phoneNumTxt.keyboardType = UIKeyboardType.phonePad
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
     @IBOutlet weak var nameTxt: UITextField!
     
     @IBOutlet weak var emailTxt: UITextField!
     
     @IBOutlet weak var passwordTxt: UITextField!
     
+    @IBOutlet weak var confirmPasswordTextField: UITextField!
+    
     @IBOutlet weak var phoneNumTxt: UITextField!
     
+    @IBAction func backButtonClicked(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
     @IBAction func registerBtn(_ sender: Any) {
-        if emailTxt.text != "" && passwordTxt.text != "" && nameTxt.text != "" && phoneNumTxt.text != "" {
+        if isValidated(){
             AuthProvider.Instance.signUp(email: emailTxt.text!, password: passwordTxt.text!, username: nameTxt.text!, phone: phoneNumTxt.text!, authHandler: { (message) in
                 if message != nil {
                     self.showAlert(message: message!)
@@ -41,21 +47,71 @@ class SignUpViewController: UIViewController {
             })
         }
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    //Validate the View Controller
+    func isValidated() -> Bool{
+        if(!isEmpty() && isPasswordMatched() && isPhoneNumberCorrect()){
+            return true
+        }
+        return false
     }
-    */
-    @IBAction func back(_ sender: Any) {
-        dismiss(animated: true, completion: nil)
+    
+    //Check if any textField is empty
+    func isEmpty() -> Bool{
+        
+        if(nameTxt.text == ""){
+            self.showAlert(message: "Please Enter Name")
+            return true
+        }
+        
+        if(emailTxt.text == ""){
+            self.showAlert(message: "Please Enter Email")
+            emailTxt.becomeFirstResponder()
+            return true
+        }
+        
+        if(passwordTxt.text == ""){
+            self.showAlert(message: "Please Enter Password")
+            passwordTxt.becomeFirstResponder()
+            return true
+        }
+        
+        if(confirmPasswordTextField.text == ""){
+            self.showAlert(message: "Please Enter Confirm Password")
+            confirmPasswordTextField.becomeFirstResponder()
+            return true
+        }
+        
+        if(phoneNumTxt.text == ""){
+            self.showAlert(message: "Please Enter Phone")
+            phoneNumTxt.becomeFirstResponder()
+            return true
+        }
+        return false
+    }
+    
+    //Checek whether the password matches the confirm password.
+    func isPasswordMatched() -> Bool{
+        if(passwordTxt.text != confirmPasswordTextField.text){
+            self.showAlert(message: "Password doesn't match Confirm Password")
+            return false
+        }
+        return true
+    }
+    
+    // Check whether the phone number is 10 digit numeric.
+    func isPhoneNumberCorrect() -> Bool {
+        let PHONE_REGEX = "\\A[0-9]{10}\\z"
+        let phoneTest = NSPredicate(format: "SELF MATCHES %@", PHONE_REGEX)
+        if(!phoneTest.evaluate(with: phoneNumTxt.text))
+        {
+            self.showAlert(message: "Invalid Phone Number")
+            return false
+        }
+        return true
     }
     
     func login() {
-        print("Test")
         if emailTxt.text != "" && passwordTxt.text != "" {
             AuthProvider.Instance.login(email: emailTxt.text!, password: passwordTxt.text!, authHandler: { (message) in
                 if message != nil {
@@ -75,3 +131,4 @@ class SignUpViewController: UIViewController {
     }
     
 }
+
