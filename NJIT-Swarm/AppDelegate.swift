1aa853781b2b3e15409c447905e7c17d26bfacc6
Palash Bairagi <Palashbairagi11122@gmail.com>
2017-11-06 01:33:00 -0500

    Likes View Controller and Comment View Controller Upload

--- NJIT-Swarm/AppDelegate.swift
+++ NJIT-Swarm/AppDelegate.swift
@@ -13,13 +13,10 @@ import Firebase
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-    var tabBar: UITabBarController?
-
+   
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
-        
-        tabBar = self.window?.rootViewController as? UITabBarController
         
         return true
     }
