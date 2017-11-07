bb0d2b49e148811faf1c9db624c63fb056cf3d0c
Apoorva Reed <apoorvareed@apoorvas-mbp.resource.campus.njit.edu>
2017-11-07 00:41:38 -0500

    added search bar feature

--- NJIT-Swarm/Controller/CheckinViewController.swift
+++ NJIT-Swarm/Controller/CheckinViewController.swift
@@ -8,18 +8,37 @@
 
 import UIKit
 import MapKit
+import Cosmos
+
+protocol HandleMapSearch {
+    func dropPinZoomIn(placemark:MKPlacemark)
+}
 
 
 class CheckinViewController: UIViewController{
+    var selectedPin:MKPlacemark? = nil
+    var lattitude: Double? = nil
+    var longitude: Double? = nil
+    var titleName: String? = nil
+    var locality: String? = nil
+    
     var currentlocation = CLLocation(latitude: 37.7749, longitude: -122.431297)
+    var resultSearchController:UISearchController? = nil
     
     
+    @IBAction func CheckInButton(_ sender: UIButton) {
+        Checkin()
+    }
+    @IBOutlet var ReviewText: UITextView!
     @IBOutlet var mapView: MKMapView!
     @IBAction func buttonCheckin(_ sender: UIBarButtonItem) {
-        Checkin()
+        //Checkin()
     }
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        
         
         let initialLocation = CLLocation(latitude: 37.7749, longitude: -122.431297)
         
@@ -30,7 +49,26 @@ class CheckinViewController: UIViewController{
         let sampleStarbucks = Venue(title: "Dummy Starbucks", locationName: "Imagination Street", coordinate: CLLocationCoordinate2D(latitude: 37.7749, longitude: -122.431297))
         mapView.addAnnotation(sampleStarbucks)
         mapView.delegate = self
+        
+    
+        
+        let locationSearchTable = storyboard!.instantiateViewController(withIdentifier: "LocationSearchTable") as! LocationSearchTable
+        resultSearchController = UISearchController(searchResultsController: locationSearchTable)
+        resultSearchController?.searchResultsUpdater = locationSearchTable
+        
+        let searchBar = resultSearchController!.searchBar
+        searchBar.sizeToFit()
+        searchBar.placeholder = "Search for places"
+        navigationItem.titleView = resultSearchController?.searchBar
+        
+        resultSearchController?.hidesNavigationBarDuringPresentation = false
+        resultSearchController?.dimsBackgroundDuringPresentation = true
+        definesPresentationContext = true
         // Do any additional setup after loading the view.
+        
+        locationSearchTable.mapView = mapView
+        locationSearchTable.handleMapSearchDelegate = self
+        
         
     }
     
@@ -52,18 +90,84 @@ class CheckinViewController: UIViewController{
                 
                 let pm = place![0]
                 
-                let title = pm.name
-                let locality = pm.locality
+//                let title = pm.name
+//                let locality = pm.locality
                 print(pm.administrativeArea!)
                 print(pm.country!)
-                let lattitude = self.currentlocation.coordinate.latitude
-                let longitude = self.currentlocation.coordinate.longitude
                 
-                // print(self.currentlocation.coordinate.latitude)
-                var LocationName :Venue
-                LocationName = Venue(title: title!, locationName: locality!, coordinate: CLLocationCoordinate2D(latitude: lattitude, longitude: longitude))
+//                let alert = UIAlertController(title: "Alert", message: "Do you want to checkin?", preferredStyle: UIAlertControllerStyle.alert)
+//                alert.addAction(UIAlertAction(title: "Cancel", style: UIAlertActionStyle.default, handler: nil))
+//                self.present(alert, animated: true, completion: nil)
+//
+//                alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: { action in
+//                    switch action.style{
+//
+//                    case .default:
+//                        print("default")
+//                    case .cancel:
+//                        print("cancel")
+//                    case .destructive:
+//                        print("destructive")
+//                    }
+//                }))
                 
-                self.mapView.addAnnotation(LocationName)
+                let alert = UIAlertController(title: "UIAlertController", message: "Would you like to Checkin?", preferredStyle: UIAlertControllerStyle.alert)
+                
+                // add the actions (buttons)
+                alert.addAction(UIAlertAction(title: "Continue", style: UIAlertActionStyle.default, handler: { action in
+                    switch action.style{
+                        
+                    case .default:
+                        print("continue")
+                        
+                        
+                        //lattitude = self.currentlocation.coordinate.latitude
+                        //longitude = self.currentlocation.coordinate.longitude
+                        let Review = self.ReviewText.text
+                        // print(self.currentlocation.coordinate.latitude)
+                        var LocationName :Venue
+                        LocationName = Venue(title: self.titleName!, locationName: self.locality!, coordinate: CLLocationCoordinate2D(latitude: self.lattitude!, longitude: self.longitude!))
+                        
+                        //                self.mapView.addAnnotation(LocationName)
+                        
+                        // to put data
+                                       DBProvider.Instance.saveCheckin(withID: AuthProvider.Instance.getUserID()!, place: self.titleName!, message: Review!, latitude: self.lattitude!, longitude: self.longitude!)
+                        
+                        self.ReviewText.text = ""
+                        
+                    case .cancel:
+                        print("byebye")
+                    case .destructive:
+                        print("destructive")
+                    }
+                }))
+                    alert.addAction(UIAlertAction(title: "Cancel", style: UIAlertActionStyle.cancel, handler: { action in
+                    switch action.style{
+                    
+                    case .default:
+                    print("cancel")
+                    case .cancel:
+                    print("bye")
+                    case .destructive:
+                    print("destructive")
+                    }
+                }))
+                
+                // show the alert
+                self.present(alert, animated: true, completion: nil)
+                
+                
+                
+               
+                // to get data
+//                UserCheckinsData.Instance.getUserCheckinsData(withID: AuthProvider.Instance.getUserID()!, handler: { (datas) in
+//                    let count = datas.count
+//                    for data in datas {
+//                        let place = data.place
+//                        let message = data.message
+//                        let numoflikes = data.numoflike
+//                    }
+//                })
                 
             }
             else {
@@ -139,3 +243,27 @@ extension CheckinViewController: MKMapViewDelegate{
     
 }
 
+
+extension CheckinViewController: HandleMapSearch {
+    func dropPinZoomIn(placemark:MKPlacemark){
+        // cache the pin
+        selectedPin = placemark
+        // clear existing pins
+        mapView.removeAnnotations(mapView.annotations)
+        let annotation = MKPointAnnotation()
+        lattitude = placemark.coordinate.latitude
+        longitude = placemark.coordinate.longitude
+        annotation.coordinate = placemark.coordinate
+        annotation.title = placemark.name
+        titleName = placemark.name!
+        locality = placemark.locality!
+        if let city = placemark.locality,
+            let state = placemark.administrativeArea {
+            annotation.subtitle = "(city) (state)"
+        }
+        mapView.addAnnotation(annotation)
+        let span = MKCoordinateSpanMake(0.05, 0.05)
+        let region = MKCoordinateRegionMake(placemark.coordinate, span)
+        mapView.setRegion(region, animated: true)
+    }
+}
