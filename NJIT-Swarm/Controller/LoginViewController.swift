365cce5772fa28db4aa7c92c64f0c6c1ccabbba0
Palash Bairagi <Palashbairagi11122@gmail.com>
2017-11-16 18:11:33 -0500

    Maintain login after closing the app

--- NJIT-Swarm/Controller/LoginViewController.swift
+++ NJIT-Swarm/Controller/LoginViewController.swift
@@ -13,13 +13,21 @@ class LoginViewController: UIViewController {
     private let HOME_PAGE_SEGUE = "homePage"
     private let SIGN_UP_SEGUE = "signUpPage"
 
+    let path = NSHomeDirectory()+"/documents/user-info.plist";
+    var dictionary: NSMutableDictionary!
+    let fileManager = FileManager.default
+    var username = ""
+    var password = ""
+    
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+ 
+        checkFile()
+        dictionary = NSMutableDictionary(contentsOfFile: path)
+        login()
     }
 
     override func didReceiveMemoryWarning() {
@@ -29,12 +37,30 @@ class LoginViewController: UIViewController {
     
     @IBAction func prepareForUnwind(segue: UIStoryboardSegue) {}
     
+    func login(){
+        AuthProvider.Instance.login(email: dictionary!.object(forKey: "username")! as! String, password: dictionary!.object(forKey: "password")! as! String, authHandler: { (message) in
+            if message != nil {
+//                self.showAlert(message: message!)
+            } else {
+                self.dictionary.setValue(self.emailTextField.text, forKey: "username")
+                self.dictionary.setValue(self.passwordTextField.text, forKey: "password")
+                self.dictionary.write(toFile: self.path, atomically: true)
+                self.emailTextField.text = ""
+                self.passwordTextField.text = ""
+                self.performSegue(withIdentifier: self.HOME_PAGE_SEGUE, sender: nil)
+            }
+        })
+    }
+    
     @IBAction func login(_ sender: Any) {
         if emailTextField.text != "" && passwordTextField.text != "" {
             AuthProvider.Instance.login(email: emailTextField.text!, password: passwordTextField.text!, authHandler: { (message) in
                 if message != nil {
                     self.showAlert(message: message!)
                 } else {
+                    self.dictionary.setValue(self.emailTextField.text, forKey: "username")
+                    self.dictionary.setValue(self.passwordTextField.text, forKey: "password")
+                    self.dictionary.write(toFile: self.path, atomically: true)
                     self.emailTextField.text = ""
                     self.passwordTextField.text = ""
                     self.performSegue(withIdentifier: self.HOME_PAGE_SEGUE, sender: nil)
@@ -46,17 +72,6 @@ class LoginViewController: UIViewController {
     } // login
     
     @IBAction func signUp(_ sender: Any) {
-//        if emailTextField.text != "" && passwordTextField.text != "" {
-//            AuthProvider.Instance.signUp(email: emailTextField.text!, password: passwordTextField.text!, authHandler: { (message) in
-//                if message != nil {
-//                    self.showAlert(message: message!)
-//                } else {
-//                    self.login(sender)
-//                }
-//            })
-//        } else {
-//            showAlert(message: AUTH_ERROR_MESSAGE.EMAIL_PASSWORD_EMPTY)
-//        }
         performSegue(withIdentifier: SIGN_UP_SEGUE, sender: nil)
     } // signUp
     
@@ -67,5 +82,16 @@ class LoginViewController: UIViewController {
         present(alert, animated: true, completion: nil)
     }
     
+    func checkFile() {
+        if !fileManager.fileExists(atPath: path) {
+            let srcPath = Bundle.main.path(forResource: "user-info", ofType: "plist")
+            do {
+                //Copy the project plist file to the documents directory.
+                try fileManager.copyItem(atPath: srcPath!, toPath: path)
+            } catch {
+                print("File copy error!")
+            }
+        }
+    }
 
 }
