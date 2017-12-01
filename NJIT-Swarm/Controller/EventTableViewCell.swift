b039823eb3437786d7cecf641197dd57b52323b2
Min Zeng <mz299@njit.edu>
2017-12-01 18:56:57 -0500

    update event page

---
+++ NJIT-Swarm/Controller/EventTableViewCell.swift
@@ -0,0 +1,45 @@
+//
+//  EventTableViewCell.swift
+//  NJIT-Swarm
+//
+//  Created by Min Zeng on 01/12/2017.
+//  Copyright © 2017 Team4. All rights reserved.
+//
+
+import UIKit
+
+class EventTableViewCell: UITableViewCell {
+    
+    private var eventData = EventData()
+    
+    @IBOutlet weak var labelEvent: UILabel!
+    @IBOutlet weak var labelLocation: UILabel!
+    @IBOutlet weak var labelTime: UILabel!
+    @IBOutlet weak var textViewDescription: UITextView!
+    @IBOutlet weak var buttonJoin: UIButton!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+    func setData(data: EventData) {
+        eventData = data
+        labelEvent.text = "Event: \(data.eventname)"
+        labelLocation.text = "Location: \(data.location)"
+        labelTime.text = "Time: \(Global.convertTimestampToDateTime(timeInterval: data.startDate))"
+        textViewDescription.text = data.description
+    }
+    
+    @IBAction func join(_ sender: Any) {
+        
+    }
+    
+    
+}
--- NJIT-Swarm/Controller/EventViewController.swift
+++ NJIT-Swarm/Controller/EventViewController.swift
@@ -8,17 +8,40 @@
 
 import UIKit
 
-class EventViewController: UIViewController {
+class EventViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet var tableView: UITableView!
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return EventsData.Instance.Data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let data = EventsData.Instance.Data[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseEventCell", for: indexPath) as! EventTableViewCell
+        cell.setData(data: data)
+        return cell
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        EventsData.Instance.update { (datas) in
+            self.tableView.dataSource = self
+            self.tableView.delegate = self
+            self.tableView.reloadData()
+        }
     }
     
 
